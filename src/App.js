67f1c2fdac0e7177ad87251af2c1c0b45a38dfe1
JuvitaSaini09 <juvitasaini@gmail.com>
2024-05-28 11:05:24 +0530

    added comment

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { AuthRoute } from "./components/authentication/authRoute";
 import {PrivateRoute} from "./components/authentication/privateRoute"
 
 function App() {
-  //home page
+  //improve design
   return (
     <div className="App">.
       <Routes>
